cb361164f7130f11d491899f6aa2fbc863c40b96
Claudinei <claudinhoalves1984@gmail.com>
2023-01-02 15:00:58 -0300

    Código asincrono com a sintaxi promises

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict"
+
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado no diretório'));
+}
+
+function pegaArquivo(caminhoDoArequivo) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArequivo, encoding)
+        .then((texto) => console.log(chalk.gray(texto)))
+        .catch(trataErro)
+    if (erro) {
+        trataErro(erro);
+    }
+    console.log(chalk.green(texto))
+}
+
+
+pegaArquivo('./texto.md')
+
+
+
+
