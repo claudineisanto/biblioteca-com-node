89e381d2bd722cdf923e19dd871bd9d258072a66
Claudinei <claudinhoalves1984@gmail.com>
2023-01-02 23:46:37 -0300

    Introdução a expressões em javascript

--- index.js
+++ index.js
@@ -3,20 +3,36 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+    const capturas = [...texto.matchAll(regex)];
+    const resutados = capturas.map(captura => ({ [captura[1]]: captura[2] }))
+    return resutados
+
+
+}
+
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Arquivo não encontrado no diretório'));
 }
 
-function pegaArquivo(caminhoDoArequivo) {
-    const encoding = 'utf-8';
-    fs.promises
-        .readFile(caminhoDoArequivo, encoding)
-        .then((texto) => console.log(chalk.gray(texto)))
-        .catch(trataErro)
+async function pegaArquivo(caminhoDoArequivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile
+            (caminhoDoArequivo, encoding);
+        console.log(extraiLinks(texto))
+    } catch (erro) {
+        trataErro(erro)
+    }
 }
 
+export default pegaArquivo;
 
-pegaArquivo('./texto.md')
+
+
 
 
 
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import pegaArquivo from "./index.js";
+
+const caminho = process.argv;
+
+pegaArquivo(caminho[2]);
+
+
